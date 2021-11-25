b9403b6f56492b8a7eb27081e479952e6037ecc7
Gustavo Hernández <gustavohdezmorales@gmail.com>
2021-11-25 14:21:06 -0600

    desarrollo 2

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import Input from '../Input/index'
 import logo from '../resources/logo512.png'
 
--- src/Input/index.js
+++ src/Input/index.js
@@ -3,21 +3,47 @@ import { getFakeData1, getFakeData2 } from '../fakeRequest'
 
 const Input = ({inputValue,onChangeInputValue}) => {
   
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  const [encontrados, setEncontrados] = useState([]);
 
-  const loadData = useMemo(async() => {
+  useMemo(async() => {
     const firstNameWithId = await getFakeData1()
     const lastNameWithId = await getFakeData2()
-    console.log({firstNameWithId, lastNameWithId});
-    setData({firstNameWithId, lastNameWithId});
-    return({firstNameWithId, lastNameWithId});
-  },[inputValue]);
   
+    const newFirstNameWithId = firstNameWithId.sort((a,b) => (a.firstName > b.firstName) ? 1 : ((b.firstName > a.firstName) ? -1 : 0));
+    const newLastNameWithId = lastNameWithId.sort((a,b) => (a.lastName > b.lastName) ? 1 : ((b.lastName > a.lastName) ? -1 : 0));
+
+    const newArray = newFirstNameWithId.map((element, index) => {
+      return({
+        id:newFirstNameWithId[index].id,
+        firstName:newFirstNameWithId[index].firstName,
+        lastName: newLastNameWithId[index].lastName
+      })
+    });
+    setData(newArray);
+    return({firstNameWithId, lastNameWithId});
+  },[]);
+  
+  useEffect(() => {
+    let newArray = [];
+    newArray = (data?.filter(element => element.firstName.toLowerCase().includes(inputValue.toLowerCase())))
+    if(newArray.length === 0) {
+      newArray = (data?.filter(element => element.lastName.toLowerCase().includes(inputValue.toLowerCase())));
+    }
+    setEncontrados(newArray);
+  }, [inputValue,data])
+
     return (
-      <input
-        onChange={e => onChangeInputValue(e.target.value)}
-        value={inputValue}
-      />
+      <>
+        <input
+          onChange={e => onChangeInputValue(e.target.value)}
+          value={inputValue}
+        />
+        {
+          inputValue !== '' && (
+            encontrados?.map((element,index) => (<p key={index}>{element.firstName} {element.lastName}</p>)))
+        }
+      </>
     )
 }
 
